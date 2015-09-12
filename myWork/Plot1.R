1ec020f3b43604a7cf8a2ff6a57a5085f83c1440
martianunlimited <me@nicklim.com>
2015-09-12 23:38:38 +1200

    Nick's Data Exploration Exercise 1
    R Code and Plots

---
+++ myWork/Plot1.R
@@ -0,0 +1,19 @@
+library(data.table)
+data=fread('household_power_consumption.txt',sep=';')
+data$Date=as.Date(data$Date,'%d/%m/%Y')
+dates =as.Date(c("2007-02-01","2007-02-02"))
+data=data[data$Date %in% dates,]
+data[,DateTime:=paste(Date,Time)]
+a=strptime(data$DateTime,'%Y-%m-%d %H:%M:%S')
+data=cbind(data,a)
+data$Global_active_power=as.numeric(data$Global_active_power)
+data$Global_reactive_power=as.numeric(data$Global_reactive_power)
+data$Voltage=as.numeric(data$Voltage)
+data$Global_intensity=as.numeric(data$Global_intensity)
+data$Sub_metering_1=as.numeric(data$Sub_metering_1)
+data$Sub_metering_2=as.numeric(data$Sub_metering_2)
+data$Sub_metering_3=as.numeric(data$Sub_metering_3)
+
+png(filename="plot1.png")
+hist(data$Global_active_power, col='red',main='Global Active Power', xlab='Global Active Power (kilowatts)', ylab='Frequency')
+dev.off();
